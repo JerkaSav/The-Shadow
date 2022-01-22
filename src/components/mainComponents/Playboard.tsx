ebd16cc5972026c966c92acb1a032c5c5e7da876
JerkSav <savstromerik@gmail.com>
2022-01-22 09:01:56 +0100

    Made logic to be able to change locations

--- src/components/mainComponents/Playboard.tsx
+++ src/components/mainComponents/Playboard.tsx
@@ -1,41 +1,49 @@
 import "../../styles/components-style/playboard.scss";
 import {PlayboardBtn} from "../playboardBtns/playboardBtn";
 import {PlayboardBtns} from "../playboardBtns/PlayboardBtns"
-import {testLocation} from "../../functions/gameLogic";
+import {location, locations} from "../../functions/gameLogic";
 import {typewriter} from "../../functions/typewriter";
 import * as React from "react";
 import {useEffect, useState} from "react";
 
 
 export const Playboard: React.FC = () => {
-    const [story, setStory] = useState<string | undefined | Promise<string | undefined>>(testLocation.next)
+    const [currentLocation, setCurrentLocation] = useState(new location(locations[0]))
+    const [story, setStory] = useState<string | undefined | Promise<string>>("Beginning")
     const [options, setOptions] = useState<string[]>()
     const progress = async () => {
-        testLocation.modifyArray()
-        await setStory(await testLocation.next)
-
+        currentLocation.modifyArray()
+        setStory(await currentLocation.next)
     }
     useEffect(() => {
         (async () => {
+            if (story?.toString().includes(">")) {
+                setStory(story?.toString().replace(">", ""))
+                setOptions(currentLocation.options)
+            }
             await typewriter(story, 20)
         })();
-        if (story?.toString().includes("Hello")) {
-            setOptions(testLocation.options)
+        if (!story) {
+            setCurrentLocation(new location(locations[1]))
         }
     }, [story]);
+    useEffect(() => {
+        setStory(currentLocation.intro[0])
+    }, [currentLocation])
     return (
         <div className="playboard">
             <div className="text-container"/>
-            {options ? <PlayboardBtns
+            {options ?
+                <PlayboardBtns
                     options={options}
                     onClick={(key) => {
-                        setStory(testLocation.answer(key))
+                        setStory(currentLocation.answer(key))
                         setOptions(undefined)
-                    }}/>
+                    }}
+                />
                 :
                 <PlayboardBtn onClick={() => progress()}/>
             }
-
         </div>
     );
 };
--- src/functions/gameLogic.ts
+++ src/functions/gameLogic.ts
@@ -1,10 +1,14 @@
-import {intro, outro, interaction} from "../story/locations/hotel.json";
+import hotel from "../story/locations/hotel.json"
+import test from "../story/locations/testing.json"
 
+export const locations = [hotel, test]
 
 type NPCType = {
-    greeting: string[],
-    options: string[],
-    answers: string[]
+    default: {
+        greeting: string[],
+        options: string[],
+        answers: string[]
+    }
 }
 type Place = {
     intro: string[],
@@ -13,10 +17,10 @@ type Place = {
 }
 
 
-class location {
-    private intro
+export class location {
+    intro
     private outro
-    private interaction
+    private readonly interaction
     private pointer = 0
     NPC: NPCType | undefined
 
@@ -25,44 +29,50 @@ class location {
         this.intro = intro
         this.outro = outro
         this.interaction = interaction
+        console.log(this.intro, this.outro, this.interaction)
 
     }
 
-    async test(action: string): Promise<string | undefined> {
+    async getNPC(): Promise<string | undefined> {
         this.NPC = await import('../story/NPC/' + this.interaction)
-        switch (action) {
-            case "Greeting":
-                return this.NPC?.greeting[0]
-        }
+        console.log(this.NPC)
+        return this.NPC?.default.greeting[0]
     }
 
-    answer(key: number) {
-        return this.NPC?.answers[key]
+    answer(key: number): string | undefined {
+        return this.NPC?.default.answers[key]
     }
 
-    get options() {
-        return this.NPC?.options
+    get options(): string[] | undefined {
+        return this.NPC?.default.options
     }
 
 
-    get next() {
+    get next(): Promise<string | undefined> | string {
         if (this.pointer != 0) {
             return this.outro[0]
         } else {
             if (!this.intro.length) {
                 this.pointer = 1
-                return this.test("Greeting")
+                return this.getNPC()
             } else {
                 return this.intro[0]
             }
         }
     }
 
-    modifyArray() {
-        if (this.pointer != 0) this.outro = this.outro.splice(1)
-        else this.intro = this.intro.splice(1)
+    modifyArray(): void {
+        if (!this.intro.length && !this.outro.length) {
+            return
+        } else {
+            if (this.pointer != 0) this.outro = this.outro.splice(1)
+            else this.intro = this.intro.splice(1)
+        }
     }
-
 }
 
-export const testLocation = new location({intro, outro, interaction})
+
+
+
+
+
